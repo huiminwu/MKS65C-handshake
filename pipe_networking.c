32f4b9b82ee1483f7fe52d83d0569c2258f2b9d0
huiminwu0 <huiminwu24@gmail.com>
2018-12-19 00:46:20 -0500

    Finish hw

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,7 +11,64 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+    if(mkfifo("wkp", 0666) == -1) {
+        perror("mkfifo");
+        exit(1);
+    } else {
+        printf("wkp server created\n");
+    }
+    
+    int up = open("wkp", O_RDONLY);
+    if (up == -1) {
+        perror("open");
+    } else {
+        printf("connected!\n");
+    }
+
+    char * buff = malloc(sizeof(char *) * BUFFER_SIZE);
+    printf("recieving client's msg\n");
+    if (read(up, buff, BUFFER_SIZE) == -1) {
+        perror("read");
+        exit(1);
+    }
+
+    printf("removing wkp\n");
+
+    printf("connecting to client fifo\n");
+    *to_client = open(buff, O_WRONLY);
+    if (*to_client != -1) {
+        printf("successful connection!\n");
+    } else {
+        printf("no connection!\n");
+    }
+
+    printf("sending ack\n");
+    if(write(*to_client, ACK, HANDSHAKE_BUFFER_SIZE) == -1) {
+        perror("write");
+        exit(1);
+    } else {
+        printf("sent initial msg!\n");
+    }
+    
+    char * cli_resp = malloc(sizeof(char *) * HANDSHAKE_BUFFER_SIZE);
+    read(up, cli_resp, HANDSHAKE_BUFFER_SIZE);
+
+    printf("======================\n");
+    printf("client's reply: %s\n", cli_resp);
+    printf("======================\n");
+    printf("donezo\n");
+
+    if(close(*to_client) == -1) {
+        perror("close");
+    } else {
+        printf("closed connections to the client\n");
+    }
+
+    free(buff);
+    free(cli_resp);
+    close(up);
+    remove("wkp");
+    return up;
 }
 
 
@@ -25,5 +82,54 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+    if(mkfifo("priv", 0666) == -1) {
+        perror("mkfifo");
+        exit(1);
+    } else {
+        printf("private fifo made!\n");
+    }
+
+    *to_server = open("wkp", O_WRONLY);
+    if (*to_server == -1) {
+        perror("open");
+    } else {
+        printf("client has connected to server\n");
+    }
+    
+    if(write(*to_server, "priv", BUFFER_SIZE) == -1) {
+        perror("write");
+        exit(1);
+    } else {
+        printf("sent private fifo name\n");
+    }
+
+    int down = open("priv", O_RDONLY);
+    char * buffer = malloc(sizeof(char *) * HANDSHAKE_BUFFER_SIZE);
+    if (*buffer == NULL) {
+        printf("error w malloc\n");
+    }
+
+    if (read(down, buffer, HANDSHAKE_BUFFER_SIZE) == -1){
+        perror("read");
+        exit(1);
+    } else {
+        printf("Received server's msg!\n");
+    }
+
+    printf("======================\n");
+    printf("server's msg: %s\n", buffer);
+    printf("======================\n");
+
+    if (write(*to_server, "hi serve", HANDSHAKE_BUFFER_SIZE) == -1) {
+        perror("write");
+        exit(1);
+    } else {
+        printf("sent response to server\n");
+    }
+    
+    free(buffer);
+    close(*to_server);
+    close(down);
+    remove("priv");
+    return down;
 }
